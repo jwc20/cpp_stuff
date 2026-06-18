04ac1c914bfec233220fed353c6f4f375975b91d
jw <choi.jae100@gmail.com>
2026-06-18 13:55:20 -0700

    add missing exercises

---
+++ zybook/1_18_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+/*
+Output should be:
+
+Step 1 complete
+Step 2 as well
+All steps now complete
+*/
+
+int main() {
+
+   cout << "Step 1 complete" << endl;
+   cout << "Step 2 as well" << endl;
+   cout << "All steps now complete" << endl;
+
+   return 0;
+}
