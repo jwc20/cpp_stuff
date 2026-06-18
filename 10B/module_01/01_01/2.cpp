fc18424674fcac2f1270510fb07c0c434ecef11e
jw <choi.jae100@gmail.com>
2026-06-18 13:15:13 -0700

    add mod 1 stuff

---
+++ 10B/module_01/01_01/2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello world!" << endl;
+  cout << "It sure is cold out here!" << endl;
+
+  cout << "Hello world!" << endl << "It's sure cold out here!" << endl;
+
+  cout << "Hello world!" << endl
+       << "It's sure cold out here!" << endl;
+}
---
+++ 10B/module_01/01_01/3.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Bob said, \"You should wear a jacket!\"" << endl;
+}
